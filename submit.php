88c471554cd94025e4230669c4c9ab676bbaa2c8
basarrak <aruk.bas01@gmail.com>
2023-11-21 10:47:13 +0700

    week 6

--- submit.php
+++ submit.php
@@ -8,15 +8,46 @@
     $sign2 = $_POST['sign2'];
     $sign3 = $_POST['sign3'];
 
-
-    echo 'email: '.$email.'<br>';
-    echo 'password: '.$password.'<br>';
-    echo 'city: '.$city.'<br>';
-    echo 'web: '.$web.'<br>';
-    echo 'role: '.$role.'<br>';
-    echo 'sign1: '.$sign1.'<br>';
-    echo 'sign2: '.$sign2.'<br>';
-    echo 'sign3: '.$sign3.'<br>';
+    $_servername = 'localhost:8080';
+    $username = 'root';
+    $password = "123456";
+    $dbname = "db_employee";
+   
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) { 
+        die("". $conn->connect_error);
+    }
+    $sql = "INSERT INTO employee (username, password, city, webserver,role)
+        VALUES ($email,$password,$city,$web,$role)";
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    // echo 'email: '.$email.'<br>';
+   // echo 'password: '.$password.'<br>';
+    // echo 'city: '.$city.'<br>';
+    // echo 'web: '.$web.'<br>';
+    // echo 'role: '.$role.'<br>';
+    // echo 'sign1: '.$sign1.'<br>';
+    // echo 'sign2: '.$sign2.'<br>';
+   // echo 'sign3: '.$sign3.'<br>';
 
 
 
