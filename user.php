0c80c345492026620fd4d264f7799a85bd6b6341
basarrak <aruk.bas01@gmail.com>
2023-11-23 00:03:15 +0700

    Week 6

--- user.php
+++ user.php
@@ -1,12 +1,19 @@
 <?php
 include 'connect.php';
-if (insstr ($_POST['submit'])){
+if(isset($_POST['submit'])){
     $name=$_POST['name'];
     $email=$_POST['email'];
     $mobile=$_POST['mobile'];
     $password=$_POST['password'];
 
-    $sql="insert into "
+    $sql="insert into `crud` (name,email,mobile,password)
+    values('$name','$email','$mobile','$password')";
+    $result=mysqli_query($con,$sql);
+    if($result){
+      echo "Data inserted successfully";
+    }else{
+        die(mysqli_error($con));
+    }
 }
 ?>
 
