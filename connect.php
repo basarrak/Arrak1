cfbcd3803eafc541c0b3f0b26bc2bb24cabcb6b9
basarrak <aruk.bas01@gmail.com>
2023-12-12 09:52:22 +0700

    Test

---
+++ connect.php
@@ -0,0 +1,25 @@
+<?php
+
+    $servername = 'localhost:8080';
+    $username = 'root';
+    $password = "";
+    $dbname = "db_employee";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("". $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO employee (username, password, city, webserver, role) 
+            VALUES ($email,$password,$city,$web,$role)";
+
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+    }  else{
+        echo"error".$sql."<br>".$conn->error;
+    }
+    $conn->close();
+    ?>
---
+++ employee.php
@@ -0,0 +1,27 @@
+<?php include  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ข้อมูลพนักงาน</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+        <H1>ข้อมูลพนักงาน</H1>
+        <tr> 
+            <table>
+            <th>Username</th> 
+            <th>City</th> 
+            <th>Webserver</th> 
+            <th>role</th> 
+            </tr>
+            <tr>
+                
+            </tr>
+            </table>
+
+           
+
+</body>
+</html>
